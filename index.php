4c33e52a114d7e876eb03b24939d4c8db68552da
Moritz Rudert (helios) <helios@planetcyborg.de>
2012-07-25 16:36:07 +0200

    text update; gallery filtering; semester and age calculation

--- index.php
+++ index.php
@@ -1,4 +1,26 @@
 <?php
+  $art_folder_rel = 'images/';
+  $art_folder = $_SERVER['DOCUMENT_ROOT'] . $art_folder_rel;
+  $default_inc = array();
+  $excludes = array('.', '..', '.ds_store', '.svn', 'thumbs', '.not_this', '.git');
+
+  if(is_dir($art_folder)) {
+    if($dh = opendir($art_folder)) {
+      while(($file = readdir($dh)) !== false) {
+        if(is_dir($art_folder . $file) && !in_array(strtolower($file), $excludes) && !file_exists($art_folder . $file . "/.not_this"))
+          array_push($default_inc, $file);
+      }
+
+      closedir($dh);
+    }
+  }
+
+  $semester = (date("Y") - 2008) * 2;
+  if(date("n") >= 10) $semester++;
+
+  $alter = (date("Y") - 1988 - 1);
+  if((date("n") == 2 && date("j") >= 2) || date("n") > 2) $alter++;
+
   if($_SERVER['HTTPS'] != "on") {
     $redirect = "https://" . $_SERVER['HTTP_HOST'] . $_SERVER['REQUEST_URI'];
     header("Location: " . $redirect);
@@ -10,15 +32,18 @@
      preg_match("/^.*radaris\.de.*$/", $_SERVER['HTTP_REFERER']))
     header("Location: /personensuchmaschine.html");
 
-switch($_REQUEST['m']) {
-  case "all":
-    $inc = "";
-    break;
+if(isset($_REQUEST['m']) && !empty($_REQUEST['m'])) {
+  switch($_REQUEST['m']) {
+    case "all":
+      $inc = "";
+      break;
 
-  default:
-    $inc = array("camp2011", "freimarkt2009", "persons", "indoorart", "office", "streetart");
-    break;
-}
+    default:
+      $inc = array($_REQUEST['m']);
+      break;
+  }
+} else
+  $inc = $default_inc;
 
 echo '<?xml version="1.0" encoding="utf-8"?>'; ?>
 <!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
@@ -48,22 +73,23 @@ echo '<?xml version="1.0" encoding="utf-8"?>'; ?>
 
       <div id="content">
 
-<h2>Wo lebe ich?</h2>
+<h2><a name="wo_lebe_ich">Wo lebe ich?</a></h2>
 <ul>
   <li>seit meiner Geburt in Bremen</li>
   <li>kurze Intermezzi in Hasbergen und Delmenhorst/Heidkrug</li>
-  <li>momentan in einer Informatiker/Soziologinnen-WG in der Neustadt</li>
-  <li>zeitweise anzutreffen: MZH; Uni-Bremen</li>
+  <li>momentan in einer <a href="http://mw.vc">Informatiker/Soziologinnen-WG</a> in der Neustadt</li>
+  <li>zeitweise anzutreffen: <a href="https://de.wikipedia.org/w/index.php?title=Datei:Mzh_uni_bremen-3.jpg">MZH</a>, Uni-Bremen</li>
 </ul>
 
-<h2>Finde mich</h2>
+<h2><a name="finde_mich">Finde mich</a></h2>
 <ul>
   <li>XMPP: <img src="https://planetcyborg.de/index.php?p=indicator&amp;jid=helios@wiresphere.de&amp;theme=0" alt="" /> <a href="xmpp:helios@wiresphere.de">helios@wiresphere.de</a></li>
   <li>IRC: helios im <a href="http://hackint.eu/">hackint</a></li>
   <li>blog: <a href="http://mw.vc/author/helios/">milliways-blog</a></li>
+  <li>E-Mail: <a href="mailto:helios@wiresphere.de">helios@wiresphere.de</a></li>
 </ul>
 
-<h2>Namen sind nur Schall und Rauch</h2>
+<h2><a name="namen">Namen sind nur Schall und Rauch</a></h2>
 <ul>
   <li><a href="http://de.wikipedia.org/wiki/Helios">helios</a></li>
   <li>chaoticbilly</li>
@@ -72,77 +98,55 @@ echo '<?xml version="1.0" encoding="utf-8"?>'; ?>
   <li>Moritz (Kaspar) Rudert</li>
 </ul>
 
-<h2>An was glaube ich?</h2>
+<h2><a name="glaube">An was glaube ich?</a></h2>
 <ul>
   <li><a href="http://de.wikipedia.org/wiki/Rastafari">Rastafarian</a></li>
   <li><a href="http://de.wikipedia.org/wiki/Cyborg">Cyborgizm</a></li>
 </ul>
 
-<h2>Was tue ich?</h2>
+<h2><a name="taten">Was tue ich?</a></h2>
 
-<h3>aktuelles Leben</h3>
+<h3><a name="taten_aktuell">aktuelles Leben</a></h3>
 <ul>
-  <li>Student an der <a href="http://uni-bremen.de/">Uni-Bremen</a> (<a href="http://informatik.uni-bremen.de/">Informatik</a> dipl.) im 7. Semester</li>
-  <li>hochschulpolitisches Engagement an der Uni-Bremen (Mitglied im StugA Informatik, verschiedene Gremienarbeit (u.A. IT-Lk (IT-Lenkungskreis)))</li>
+  <li><?php echo $alter; ?> Jahre alt</li>
+  <li>Student an der <a href="http://uni-bremen.de/">Uni-Bremen</a> (<a href="http://informatik.uni-bremen.de/">Informatik</a> dipl.) im <?php echo $semester; ?>. Semester</li>
+  <li>Versuch als <a href="https://de.wikipedia.org/wiki/Pescetarismus">Ovo-Lacto-Pesco-Vegetarier</a> zu leben</li>
+  <li>hochschulpolitisches Engagement an der Uni-Bremen:
+  <ul style="margin-bottom: 0">
+    <li>Mitglied im <a href="http://inf.stugen.de/">StugA Informatik</a></li>
+    <li>Stugen-Admin</li>
+    <li>Gremienarbeit: IT-Lk (IT-Lenkungskreis)</li>
+    <li>Gremienarbeit: Stugenkonferenz</li>
+  </ul></li>
   <li><a href="http://www.wlan.uni-bremen.de/">Campus-WLAN</a>-Betreuung als studentische Hilfskraft</li>
   <li>eigene Firma seit 2007 (<a href="http://planetcyborg.de/">planet cyborg</a> vormals Quasar-Net)</li>
-  <li><a href="http://wybt.net/">wybt</a></li>
-  <li>verschiedene <a href="http://ccc.de/">CCC</a>-nahe Aktivit&auml;ten (u.A. <a href="http://dn42.net/">dn42</a>)</li>
+  <li><a href="http://wybt.net/">wybt</a>-Admin</li>
+  <li>Vereinsmitglied im <a href="http://fbmi.de/">FBMI</a> (F&ouml;rderverein der Bereichsstudierendenschaften Mathematik und Informatik an der Universit&auml;t Bremen e.V.)</li>
   <li>Vereinsmitglied im <a href="http://ccc.de/">CCC</a></li>
-  <li>Vereinsmitglied im FBMI (F&ouml;rderverein der Bereichsstudierendenschaften Mathematik und Informatik an der Universit&auml;t Bremen e.V.)</li>
+  <li>verschiedene <a href="http://ccc.de/">CCC</a>-nahe Aktivit&auml;ten<ul style="margin-bottom: 0">
+    <li><a href="http://dn42.net/">dn42</a>-Gr&uuml;nder, Eigent&uuml;mer der Domain und Administrator des tracs</li>
+    <li>Developer und Admin vom <a href="https://engelsystem.de/">engelsystem</a> (source: <a href="https://planetcyborg.de/git/projects/engelsystem.git">planetcyborg.de/git/projects/engelsystem.git</a>)</li>
+  </ul></li>
+  <li>Vereinsmitglied im <a href="http://hackerspace-bremen.de/">Hackerspace Bremen</a><ul style="margin-bottom: 0">
+    <li>Administrator des Bremer Hackerspace Servers</li>
+  </ul></li>
   <li>Webmaster vom <a href="http://hackint.eu/">hackint</a></li>
   <li>Initiator des <a href="http://jabber.uni-bremen.de/">Jabberservers</a> der Uni-Bremen</li>
-  <li>Vereinsmitglied im <a href="http://hackerspace-bremen.de/">Hackerspace Bremen</a></li>
 </ul>
 
-<h2>art</h2>
-<div id="gallery">
-<?php
-
-$dir = "./images/";
-$excludes = array('.', '..', '.ds_store', '.svn', 'thumbs');
-
-// Open a known directory, and proceed to read its contents
-echo "<ul id=\"art\">";
-if (is_dir($dir)) {
-  if ($dh = opendir($dir)) {
-    while (($file = readdir($dh)) !== false) {
-      if(!in_array(strtolower($file), $excludes) && is_dir($dir . $file)) {
-        if (is_dir($dir . $file) && (!is_array($inc) || in_array($file, $inc))) {
-          if ($dh2 = opendir($dir . $file)) {
-            while (($file2 = readdir($dh2)) !== false) {
-              if(!in_array(strtolower($dir . $file . "/" . $file2), $excludes) && is_file($dir . $file . "/" . $file2)) {
-                if(!file_exists($dir . "thumbs/" . $file . "_" . $file2))
-                  system("convert -resize 120x120 \"" . $dir . $file . "/" . $file2 . "\" \"" . $dir . "thumbs/" . $file . "_" . $file2 . "\"");
-
-                echo "<li><a href=\"" . $dir . $file . "/" . $file2 . "\"><img src=\"" . $dir . "thumbs/" . $file . "_" . $file2 . "\" alt=\"\" title=\"" . basename($file2) . "\"/></a></li>\n";
-              }
-            }
-
-            closedir($dh2);
-          }
-        }
-      }
-    }
-
-    closedir($dh);
-  }
-}
-?>
-</ul>
-</div>
-
-<h3>vergangenes Leben</h3>
+<h3><a name="taten_vergangen">vergangenes Leben</a></h3>
 <ul>
-  <li>Mitorganisator der KIF 39.5/KoMa 69 in Bremen</li>
-  <li>zweimaliger Weltmeister im <a href="http://www.robocupgermanopen.de/">Robocup</a> <a href="http://www.robocupgermanopen.de/en/junior/rescue">junior rescue</a> 2008/2009 
+  <li>NOC und Stromorga f&uuml;r den <a href="http://www.informatik.uni-bremen.de/projekttag/2012/">Projekttag Informatik 2012</a> an der Uni-Bremen</li>
+  <li>Webmaster f&uuml;r den <a href="http://www.informatik.uni-bremen.de/projekttag/2012/">Projekttag Informatik 2012</a> an der Uni-Bremen</li>
+  <li>Mitorganisator der <a href="http://kif.fsinf.de/wiki/KIF395:Hauptseite">KIF 39.5</a>/KoMa 69 in Bremen</li>
+  <li>zweimaliger Weltmeister im <a href="http://www.robocupgermanopen.de/">Robocup</a> <a href="http://junior.robocupgermanopen.de/junior/rescue">junior rescue</a> 2008/2009 
     (einmal als Teammitglied, einmal als Betreuer im Team <a href="/hoppus/">hoppuS</a>)</li>
   <li>verschiedenes politisches Engagement in Bremen (OL (Ortsleitung) der <a href="http://www.naturfreundejugend.de/">NFJ</a>-Bremen)</li>
-  <li>Sch&uuml;ler am SZ Walle Lange Reihe Sek. 2<ul>
+  <li>Sch&uuml;ler am SZ Walle Lange Reihe Sek. 2<ul style="margin-bottom: 0">
     <li>SV-Vorstand/Schulsprecher des SZ Walle Lange Reihe Sek. 2</li>
     <li>Erstellung der Schulhomepage SZ Walle Lange Reihe Sek. 2</li>
   </ul></li>
-  <li>Sch&uuml;ler des SZ Findorff<ul>
+  <li>Sch&uuml;ler des SZ Findorff<ul style="margin-bottom: 0">
     <li>Betreuung der Schulrechner des SZ Findorff</li>
     <li>Sch&uuml;lervertreter am SZ Findorff</li>
   </ul></li>
@@ -150,14 +154,15 @@ if (is_dir($dir)) {
   <li>Sch&uuml;ler der Grundschule SZ Findorff Admiralstr.</li>
 </ul>
 
-<h3>zuk&uuml;nftiges Leben</h3>
+<h3><a name="taten_zukuenftig">zuk&uuml;nftiges Leben</a></h3>
 <ul>
   <li>Studium abschlie&szlig;en</li>
   <li>technischer Mitarbeiter an der Uni-Bremen</li>
   <li><a href="http://de.wikipedia.org/wiki/Das_Haus_am_See">Haus</a> am <a href="http://de.wikipedia.org/wiki/Stadtaffe">See</a></li>
 </ul>
 
-<h2>andere &uuml;ber mich</h2>
+<h2><a name="andere">andere &uuml;ber mich</a></h2>
+<small>Willst du, dass deine Aussage auch hier steht? Dann schick mir eine E-Mail an <a href="mailto:andere@wiresphere.de">andere@wiresphere.de</a>.</small><br />
 <ul>
   <li>von den Leuten, die ich bisher getroffen hab, mit die meiste Ahnung von Servern, Netzen und &Auml;hnlichem.</li>
   <li>Mit ihm arbeiten macht Spa&szlig;, ist am Anfang aber ungewohnt, denn er sagt, was er denkt; manchmal auf eine Art, an die man sich erst gew&ouml;hnen muss.</li>
@@ -174,7 +179,39 @@ if (is_dir($dir)) {
   <li>Hat mehr Dinge im Kopf, als ich K&ouml;pfe habe.</li>
 </ul>
 
-<h2>hier lese ich</h2>
+<h2><a name="art">art</a></h2>
+<div id="gallery">
+<?php
+echo "<ul id=\"art\">";
+if (is_dir($art_folder)) {
+  if ($dh = opendir($art_folder)) {
+    while (($file = readdir($dh)) !== false) {
+      if(!in_array(strtolower($file), $excludes) && is_dir($art_folder . $file)) {
+        if (!is_array($inc) || in_array($file, $inc)) {
+          if ($dh2 = opendir($art_folder . $file)) {
+            while (($file2 = readdir($dh2)) !== false) {
+              if(!in_array(strtolower($file2), $excludes) && is_file($art_folder . $file . "/" . $file2)) {
+                if(!file_exists($art_folder . "thumbs/" . $file . "_" . $file2))
+                  system("convert -resize 120x120 \"" . $art_folder . $file . "/" . $file2 . "\" \"" . $art_folder . "thumbs/" . $file . "_" . $file2 . "\"");
+
+                echo "<li><a href=\"" . $art_folder_rel . $file . "/" . $file2 . "\"><img src=\"" . $art_folder_rel . "thumbs/" . $file . "_" . $file2 . "\" alt=\"\" title=\"" . basename($file2) . "\"/></a></li>\n";
+              }
+            }
+
+            closedir($dh2);
+          }
+        }
+      }
+    }
+
+    closedir($dh);
+  }
+}
+?>
+</ul>
+</div>
+
+<h2><a name="lese">hier lese ich</a></h2>
 <ul>
   <li><a href="http://mieke.planetcyb.org/">mieke</a></li>
   <li><a href="http://tahlly.planetcyb.org/">tahlly</a></li>
@@ -186,10 +223,10 @@ if (is_dir($dir)) {
   <li><a href="https://blog.jplitza.de/">jplitza</a></li>
 </ul>
 
-<h2>Tracking</h2>
+<h2><a name="tracking">Tracking</a></h2>
 <p><iframe width="560px" height="250px" src="http://planetcyborg.de/piwik/index.php?module=CoreAdminHome&#038;action=optOut&#038;language=de" style="background: #fff;"></iframe></p>
 
-<h2>Impressum</h2>
+<h2><a name="impressum">Impressum</a></h2>
 <p>Alleinverantwortlich für diese Internetseite ist:</p>
 <p>Moritz Kaspar Rudert<br />
 Hohentorsheerstr. 172<br />
@@ -217,7 +254,7 @@ piwikTracker.enableLinkTracking();
 <script type="text/javascript">
   Galleria.loadTheme('galleria/themes/classic/galleria.classic.min.js');
   $("#gallery").galleria({
-    width: 500,
+    width: 880,
     height: 500
   });
 </script>
